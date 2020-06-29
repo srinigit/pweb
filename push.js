8557e079c0ba160e844d61fa529600e177495a8e
srinigit <maddali.srinivas@gmail.com>
2020-06-29 10:01:11 -0600

    Update push.js

--- push.js
+++ push.js
@@ -12,15 +12,6 @@ push.setVapidDetails(
   vapidKeys.privateKey
 );
 
-let sub = {
-  endpoint:
-    'https://fcm.googleapis.com/fcm/send/fwMG_ImcKYc:APA91bHtTnPUQ0tBehI7Kk1CGn4s3yhIKhJunhqiggl5XxbyVPmqxDkugwctQMzL2VDTrG_ypKwWqb1Djv-kqWbxpmsatPSjSAUldFENqCIZShaFvZFiCwH-X2hS0-Xjqv5mq2XYHcf9',
-  expirationTime: null,
-  keys: {
-    p256dh:
-      'BLHrTAa9dZ4LV5DoCgtXCXLMd21_S3qyt4eSqn6UQCjIEAuemY0QI29Mvc-LL02e0DLGPKd3xkVHjCD4eqJWHSg',
-    auth: 'TW-LBZmYtXNW7VQUPX4I0w'
-  }
-};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d9iQiZztMCo:APA91bFMN42TW9VcTrmhZYAXzHq_xu-Ne2P1mgr_gEZIZidedvIxRFf4CND1KwVvOX9scsgT7igiN_z6Wqv_D2JBNq4rr9JSIIr1grfJMCGF_8ofTYdpbM1E9TLDy_V1c7BdjZXIWz3I","expirationTime":null,"keys":{"p256dh":"BDFL1Vq3a_tLnSAUR9xpNnV-eTOxn3Hh5U2tBs0sjhaWdmJ_g2DaVXtNpB1mqKU79iUcRvhQSxenLFDRChrVHI4","auth":"dk3pTjXlAW-ltFLnWDsqvg"}};
 
 push.sendNotification(sub, 'test message');
